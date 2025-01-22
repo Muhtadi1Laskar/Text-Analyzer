ebc13bd875a8a8b49b1e07c446f06885483774f9
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-01-22 14:11:21 +0600

    Removed comments

--- handlers/helper.go
+++ handlers/helper.go
@@ -23,18 +23,15 @@ type ErrorResponse struct {
 
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Add CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		// Handle preflight requests
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 
-		// Pass to the next handler
 		next.ServeHTTP(w, r)
 	})
 }
