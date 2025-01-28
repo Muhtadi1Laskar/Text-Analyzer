0a6d0930ddb2ba3d60934887a78cc4398d57be99
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-01-28 10:21:43 +0600

    Implemented feature extraction for plagirasm checker

--- core/textAnalyer.go
+++ core/textAnalyer.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"regexp"
 	"strings"
 	"unicode"
 )
@@ -12,6 +13,13 @@ type Analysis struct {
 	SentenceCount    int
 	AverageWordCount float32
 	TotalStopWords   int
+}
+
+type FeatureVector struct {
+	AvgWordLength      float64
+	AvgSentenceLength  float64
+	StopWordFrequency  float64
+	VocabularyRichness float64
 }
 
 func countWord(text string) int {
@@ -64,7 +72,7 @@ func countSentence(text string) int {
 
 func countStopWords(text string) int {
 	text = RemovePunctuation(text)
-	tokenizeText := strings.Fields(text)
+	tokenizeText := Tokenize(text)
 
 	var totalStopWords int = 0
 	for _, elem := range tokenizeText {
@@ -75,12 +83,63 @@ func countStopWords(text string) int {
 	return totalStopWords
 }
 
+func Preprocess(text string) []string {
+	text = RemovePunctuation(text)
+	tokens := Tokenize(text)
+
+	return tokens
+}
+
+func ExtractFeature(text string) FeatureVector {
+	tokens := Preprocess(text)
+	sentences := splitSentences(text)
+
+	if len(tokens) == 0 || len(sentences) == 0 {
+		return FeatureVector{}
+	}
+
+	totalWordLength := 0
+	for _, word := range tokens {
+		totalWordLength += len(word)
+	}
+	avgWordLength := float64(totalWordLength) / float64(len(tokens))
+	avgSentenceLength := float64(len(tokens)) / float64(len(sentences))
+	totalStopWords := countStopWords(text)
+	stopwordFreq := float64(totalStopWords) / float64(len(tokens))
+
+	uniqueWords := make(map[string]bool)
+	for _, word := range tokens {
+		uniqueWords[word] = true
+	}
+	vocabRichness := float64(len(uniqueWords)) / float64(len(tokens))
+
+	return FeatureVector{
+		AvgWordLength: avgWordLength,
+		AvgSentenceLength: avgSentenceLength,
+		StopWordFrequency: stopwordFreq,
+		VocabularyRichness: vocabRichness,
+	}
+}
+
 func averageWordCount(text string) float32 {
 	return float32(characterCount(text)) / float32(countWord(text))
 }
 
 func isWhiteSpace(char rune) bool {
 	return char == ' ' || char == '\n' || char == '\t'
+}
+
+func splitSentences(text string) []string {
+	reg := regexp.MustCompile(`[.!?]+`)
+	sentences := reg.Split(text, -1)
+
+	var result []string
+	for _, sentence := range sentences {
+		if strings.TrimSpace(sentence) != "" {
+			result = append(result, sentence)
+		}
+	}
+	return result
 }
 
 func MainFunc(text string) Analysis {
--- handlers/plagrism_handlers.go
+++ handlers/plagrism_handlers.go
@@ -6,13 +6,22 @@ import (
 )
 
 type PlagrismRequest struct {
-	TextOne string `json:"textOne" validate:"required"`
-	TextTwo string `json:"textTwo" validate:"required"`
+	TextOne     string `json:"textOne" validate:"required"`
+	TextTwo     string `json:"textTwo" validate:"required"`
 	CheckerType string `json:"checkerType" validate:"required"`
 }
 
 type PlagrismResponse struct {
-	Similarity float64 `json:"similarity" validate:"required"`
+	Similarity         float64  `json:"similarity" validate:"required"`
+	KnownText          Features `json:"knownText" validate:"required"`
+	UnknownText        Features `json:"unknownText" validate:"required"`
+}
+
+type Features struct {
+	AvgWordLength      float64 `json:"avgWordLength"`
+	AvgSentenceLength  float64 `json:"avgSentenceLength"`
+	StopWordFrequency  float64 `json:"stopWordFrequency"`
+	VocabularyRichness float64 `json:"vocabularyRichness"`
 }
 
 func PlagrismChecker(w http.ResponseWriter, r *http.Request) {
@@ -22,27 +31,42 @@ func PlagrismChecker(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	
 	fileTwo, err := UploadFile(r, "fileTwo")
 	if err != nil {
 		writeError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
+	textOne, textTwo := fileOne["message"], fileTwo["message"]
+
 	var similarity float64
 	switch fileOne["checkerType"] {
 	case "cosine-similarity":
-		similarity = core.CheckPlagrism(fileOne["message"], fileTwo["message"])
+		similarity = core.CheckPlagrism(textOne, textTwo)
 	case "minhash":
-		similarity = core.MinHash(fileOne["message"], fileTwo["message"])
+		similarity = core.MinHash(textOne, textTwo)
 	default:
 		writeError(w, http.StatusInternalServerError, "Invalid Checker Type")
 		return
 	}
 
+	textOneFeatures := core.ExtractFeature(textOne)
+	textTwoFeatures := core.ExtractFeature(textTwo)
 
 	response := PlagrismResponse{
 		Similarity: similarity,
+		KnownText: Features{
+			AvgWordLength:      textOneFeatures.AvgWordLength,
+			AvgSentenceLength:  textOneFeatures.AvgSentenceLength,
+			StopWordFrequency:  textOneFeatures.StopWordFrequency,
+			VocabularyRichness: textOneFeatures.VocabularyRichness,
+		},
+		UnknownText: Features{
+			AvgWordLength:      textTwoFeatures.AvgWordLength,
+			AvgSentenceLength:  textTwoFeatures.AvgSentenceLength,
+			StopWordFrequency:  textTwoFeatures.StopWordFrequency,
+			VocabularyRichness: textTwoFeatures.VocabularyRichness,
+		},
 	}
 
 	writeJSONResponse(w, http.StatusOK, response)
